6a70581fac441a30c90161facc9a128b4eb8fad7
natha9404 <nathalia.bedoya@correounivalle.edu.co>
2015-09-22 21:23:15 -0500

    Update echo-client.py

--- echo-client.py
+++ echo-client.py
@@ -5,7 +5,7 @@
 # como su nombre lo sugiere quiere decir que lo que recibe el servidor lo 
 # regresa tal cual al cliente. Si el cliente envia un 'hola mundo' el servidor
 # le regresara un 'hola mundo'.
-#
+#natha
 # En este programa el cliente digitara una cadena se la enviara al servidor
 # y este enviara la cadena de vuelta en pedazos de 16 bytes.
 #
